a298577b25794df9253febb577f0f2e45ae11408
陈海飞 <15378702772@163.com>
2022-05-13 17:06:16 +0800

    build(babel): add babel

--- dist/index.js
+++ dist/index.js
@@ -1,166 +1,75 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+"use strict";
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ ((module, __webpack_exports__, __webpack_require__) => {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/classCallCheck */ \"@babel/runtime/helpers/classCallCheck\");\n/* harmony import */ var _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/createClass */ \"@babel/runtime/helpers/createClass\");\n/* harmony import */ var _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1__);\n/* module decorator */ module = __webpack_require__.hmd(module);\n\n\n\nvar path = __webpack_require__(/*! path */ \"path\");\n\nvar Colorette = __webpack_require__(/*! colorette */ \"colorette\");\n\nvar _require = __webpack_require__(/*! node-ssh */ \"node-ssh\"),\n    NodeSSH = _require.NodeSSH;\n\nvar FileUploadWebpackPlugin = /*#__PURE__*/function () {\n  function FileUploadWebpackPlugin(options) {\n    _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0___default()(this, FileUploadWebpackPlugin);\n\n    this.ssh = new NodeSSH();\n    this.color = Colorette;\n    this.options = options;\n  }\n\n  _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1___default()(FileUploadWebpackPlugin, [{\n    key: \"apply\",\n    value: function apply(compiler) {\n      var _this = this;\n\n      compiler.hooks.afterEmit.tap('FileUploadWebpackPlugin', function (compilation) {\n        var outputPath = compilation.outputOptions.path;\n        var port = _this.options.port || '22';\n        var sshCommand = \"ssh \".concat(_this.options.username, \"@\").concat(_this.options.host, \" -p \").concat(port); // ssh connect\n\n        _this.ssh.connect({\n          host: _this.options.host,\n          port: port,\n          username: _this.options.username,\n          privateKey: _this.options.privateKey\n        }).then(function () {\n          console.log(\"--> \".concat(_this.color.green(sshCommand), \" login success\"));\n          console.log(\"--> Uploading file...\"); // ssh putDirectory\n\n          _this.ssh.putDirectory(outputPath, _this.options.remotePath, {\n            recursive: true,\n            concurrency: _this.options.concurrency || 10,\n            tick: function tick(localPath, remotePath, error) {\n              if (error) {\n                console.log(\"\".concat(_this.color.red('X'), \" uploaded file \").concat(_this.color.red(path.basename(localPath))));\n              } else {\n                console.log(\"\".concat(_this.color.green('✔'), \" uploaded file \").concat(_this.color.green(path.basename(localPath))));\n              }\n            }\n          }).then(function (status) {\n            console.log('File uploaded', status ? _this.color.green('successfully') : _this.color.red('failed'));\n\n            _this.ssh.dispose();\n          }).catch(function (err) {\n            console.log(\"--> \".concat(sshCommand, \" put directory fail:\"), err);\n\n            _this.ssh.dispose();\n          });\n        }).catch(function (err) {\n          console.log(\"--> \".concat(sshCommand, \" login fail:\"), err);\n\n          _this.ssh.dispose();\n        });\n      });\n    }\n  }]);\n\n  return FileUploadWebpackPlugin;\n}();\n\nmodule.exports = FileUploadWebpackPlugin;\n\n//# sourceURL=webpack://file-upload-webpack-plugin/./src/index.js?");
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-/***/ }),
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
-/***/ "@babel/runtime/helpers/classCallCheck":
-/*!********************************************************!*\
-  !*** external "@babel/runtime/helpers/classCallCheck" ***!
-  \********************************************************/
-/***/ ((module) => {
+var path = require('path');
 
-module.exports = require("@babel/runtime/helpers/classCallCheck");
+var Colorette = require('colorette');
 
-/***/ }),
+var _require = require('node-ssh'),
+    NodeSSH = _require.NodeSSH;
 
-/***/ "@babel/runtime/helpers/createClass":
-/*!*****************************************************!*\
-  !*** external "@babel/runtime/helpers/createClass" ***!
-  \*****************************************************/
-/***/ ((module) => {
+var FileUploadWebpackPlugin = /*#__PURE__*/function () {
+  function FileUploadWebpackPlugin(options) {
+    _classCallCheck(this, FileUploadWebpackPlugin);
 
-module.exports = require("@babel/runtime/helpers/createClass");
+    this.ssh = new NodeSSH();
+    this.color = Colorette;
+    this.options = options;
+  }
 
-/***/ }),
+  _createClass(FileUploadWebpackPlugin, [{
+    key: "apply",
+    value: function apply(compiler) {
+      var _this = this;
 
-/***/ "colorette":
-/*!****************************!*\
-  !*** external "colorette" ***!
-  \****************************/
-/***/ ((module) => {
+      compiler.hooks.afterEmit.tap('FileUploadWebpackPlugin', function (compilation) {
+        var outputPath = compilation.outputOptions.path;
+        var port = _this.options.port || '22';
+        var sshCommand = "ssh ".concat(_this.options.username, "@").concat(_this.options.host, " -p ").concat(port); // ssh connect
 
-module.exports = require("colorette");
+        _this.ssh.connect({
+          host: _this.options.host,
+          port: port,
+          username: _this.options.username,
+          privateKey: _this.options.privateKey
+        }).then(function () {
+          console.log("--> ".concat(_this.color.green(sshCommand), " login success"));
+          console.log("--> Uploading file..."); // ssh putDirectory
 
-/***/ }),
+          _this.ssh.putDirectory(outputPath, _this.options.remotePath, {
+            recursive: true,
+            concurrency: _this.options.concurrency || 10,
+            tick: function tick(localPath, remotePath, error) {
+              if (error) {
+                console.log("".concat(_this.color.red('X'), " uploaded file ").concat(_this.color.red(path.basename(localPath))));
+              } else {
+                console.log("".concat(_this.color.green('✔'), " uploaded file ").concat(_this.color.green(path.basename(localPath))));
+              }
+            }
+          }).then(function (status) {
+            console.log('File uploaded', status ? _this.color.green('successfully') : _this.color.red('failed'));
 
-/***/ "node-ssh":
-/*!***************************!*\
-  !*** external "node-ssh" ***!
-  \***************************/
-/***/ ((module) => {
+            _this.ssh.dispose();
+          })["catch"](function (err) {
+            console.log("--> ".concat(sshCommand, " put directory fail:"), err);
 
-module.exports = require("node-ssh");
+            _this.ssh.dispose();
+          });
+        })["catch"](function (err) {
+          console.log("--> ".concat(sshCommand, " login fail:"), err);
 
-/***/ }),
+          _this.ssh.dispose();
+        });
+      });
+    }
+  }]);
 
-/***/ "path":
-/*!***********************!*\
-  !*** external "path" ***!
-  \***********************/
-/***/ ((module) => {
+  return FileUploadWebpackPlugin;
+}();
 
-module.exports = require("path");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			id: moduleId,
-/******/ 			loaded: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/harmony module decorator */
-/******/ 	(() => {
-/******/ 		__webpack_require__.hmd = (module) => {
-/******/ 			module = Object.create(module);
-/******/ 			if (!module.children) module.children = [];
-/******/ 			Object.defineProperty(module, 'exports', {
-/******/ 				enumerable: true,
-/******/ 				set: () => {
-/******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
-/******/ 				}
-/******/ 			});
-/******/ 			return module;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
-/******/ 	
-/******/ })()
-;
+module.exports = FileUploadWebpackPlugin;
